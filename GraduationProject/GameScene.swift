b0b776990024d416e1cab8a1990c50ca80fe338b
Ali Faris Akdem <faris.akdem@ozu.edu.tr>
2017-12-18 08:11:34 -0800

    Friction data
    storing Friction data in Matrix and debugging

--- GraduationProject/GameScene.swift
+++ GraduationProject/GameScene.swift
@@ -17,15 +17,24 @@ class GameScene: SKScene {
     var manager: CMMotionManager?
     var timer: Timer?
     var seconds: Double?
-    var frictionMap: [[Double]] = []
+    //var frictionMap: [[[Double]]]  = [[[0.0]],[[0.0]],[[0.0]]]//i,j,friction.
+    
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    let deviceWidth = 750.0
     let deviceHeight = 1334.0
+    let deviceWidth = 750.0
     
     func logPixelsOf(_ image: UIImage) {
+        /* override init() {
+         setFriction(#imageLiteral(resourceName: "friction"))
+         }
+         
+         required init?(coder aDecoder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+         }*/
+        print("1")
         // 1. Get pixels of image
         let inputCGImage = image.cgImage
         let width: Int = (inputCGImage?.width)!
@@ -80,9 +89,45 @@ class GameScene: SKScene {
                 currentPixel += 1
             }
         }
+        
     }
     
-    func getFriction(_ image: UIImage) {
+    func setFriction(_ image: UIImage) {
+        
+        //https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Subscripts.html   alındı.
+        struct Matrix {
+            let rows: Int, columns: Int
+            var grid: [Double]
+            init(rows: Int, columns: Int) {
+                self.rows = rows
+                self.columns = columns
+                grid = Array(repeating: 0.0, count: rows * columns)
+            }
+            func indexIsValid(row: Int, column: Int) -> Bool {
+                return row >= 0 && row < rows && column >= 0 && column < columns
+            }
+            subscript(row: Int, column: Int) -> Double {
+                get {
+                    assert(indexIsValid(row: row, column: column), "Index out of range")
+                    return grid[(row * columns) + column]
+                }
+                set {
+                    assert(indexIsValid(row: row, column: column), "Index out of range")
+                    grid[(row * columns) + column] = newValue
+                }
+            }
+        }
+        var matrix = Matrix(rows: 750, columns: 1334)
+        
+        
+        
+        
+        
+        
+        
+        print("2")
+        //var friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
+        
         // 1. Get pixels of image
         let inputCGImage = image.cgImage
         let width: Int = (inputCGImage?.width)!
@@ -113,30 +158,40 @@ class GameScene: SKScene {
         }
         
         var currentPixel = pixels
-        for j in 0..<height {
-            for i in 0..<width {
+        for j in 0..<height {// y coordinate
+            for i in 0..<width {// x coordinate
+                //var friction: [Double] = [0.0]
                 let color = currentPixel.pointee.hashValue
                 let alpha = A(x: color)
                 if (alpha == 0){
-                    frictionMap[j][i] = 1.0
+                    //friction = [0.5]
+                    matrix[i,j] = 0.5
                 }else if (alpha <= 20 ){
-                    frictionMap[j][i] = 0.9
+                    matrix[i,j] = 0.9
+                    //frictionMap[i][j] = 0.9
                 }else if (alpha <= 50 ){
-                    frictionMap[j][i] = 0.8
+                    matrix[i,j] = 0.8
+                    //frictionMap[i][j] = 0.8
                 }else if (alpha <= 100 ){
-                    frictionMap[j][i] = 0.7
+                    matrix[i,j] = 0.7
+                    //frictionMap[i][j] = 0.7
                 }else if (alpha <= 200 ){
-                    frictionMap[j][i] = 0.3
+                    matrix[i,j] = 0.3
+                    //frictionMap[i][j] = 0.3
                 }
+                //var temp = frictionMap[i][j][friction]
                 currentPixel += 1
+                //print("currentPixel: ",currentPixel, "alpha:" ,alpha, "frictionMap:" ,matrix, "i:" ,i, "j:" ,j)
             }
         }
+        print("frictionMap:" ,matrix)
     }
     
     
     override func didMove(to view: SKView) {
-
+        print("3")
         logPixelsOf(#imageLiteral(resourceName: "obstaclex4"))
+        setFriction(#imageLiteral(resourceName: "friction"))
         
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(GameScene.increaseTimer), userInfo: nil, repeats: true)
         
@@ -161,48 +216,57 @@ class GameScene: SKScene {
     
     
     func touchDown(atPoint pos : CGPoint) {
-
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
+        print("4")
         if let gravityX = manager?.deviceMotion?.gravity.x, let gravityY = manager?.deviceMotion?.gravity.y, ball != nil {
+            /* 18.12.2017 Faris Commented
+             let friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
+             */
+            
             // let newPosition = CGPoint(x: Double(ball.position.x) + gravityX * 35.0, y: Double(ball.position.y) + gravityY * 35.0)
             // let moveAction = SKAction.moveTo(newPosition, duration: 0.0)
             // ball.runAction(moveAction)
             
             // applyImpulse() is much better than applyForce()
             // ball.physicsBody?.applyForce(CGVector(dx: CGFloat(gravityX) * 5000.0, dy: CGFloat(gravityY) * 5000.0))
-            getFriction(#imageLiteral(resourceName: "friction"))
-            var friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
-            ball.physicsBody?.applyImpulse(CGVector(dx: CGFloat(gravityX) * 200.0 * CGFloat(friction), dy: CGFloat(gravityY) * 200.0 * CGFloat(friction)))
+            
+            
+            // var friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
+            /* 18.12.2017 Faris Commented
+             ball.physicsBody?.applyImpulse(CGVector(dx: CGFloat(gravityX) * 200.0 * CGFloat(friction), dy: CGFloat(gravityY) * 200.0 * CGFloat(friction)))
+             */
         }
     }
     
     func centerBall() {
+        print("5")
         ball.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
         let moveAction = SKAction.move(to: CGPoint(x: frame.midX, y: frame.midY), duration: 0.0)
         ball.run(moveAction)
@@ -211,13 +275,15 @@ class GameScene: SKScene {
     // MARK: - Timer Methods
     
     @objc func increaseTimer() {
+        print("6")
         seconds = (seconds ?? 0.0) + 0.01
     }
     
     func resetTimer() {
+        print("7")
         seconds = 0.0
     }
-
+    
 }
 
 
