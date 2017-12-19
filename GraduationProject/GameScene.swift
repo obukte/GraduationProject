c091c49f4a9575f6a2e9999c6f4d2600f42b692e
obukte <omer.bukte@ozu.edu.tr>
2017-12-20 00:12:51 +0300

    Friction
    Friction function completed for some parts

--- GraduationProject/GameScene.swift
+++ GraduationProject/GameScene.swift
@@ -123,30 +123,33 @@ class GameScene: SKScene {
         for j in 0..<height {// y coordinate
             for i in 0..<width {// x coordinate
                 let color = currentPixel.pointee.hashValue
+                let red = R(x: color)
+                let green = G(x: color)
+                let blue = B(x: color)
                 let alpha = A(x: color)
-                if (alpha == 0){
-                    frictionMap[i][j] = 0.5
-                }else if (alpha <= 20 ){
-                    frictionMap[i][j] = 0.9
-                }else if (alpha <= 50 ){
-                    frictionMap[i][j] = 0.8
-                }else if (alpha <= 100 ){
-                    frictionMap[i][j] = 0.7
-                }else if (alpha <= 200 ){
-                    frictionMap[i][j] = 0.3
+                if (red < 150 ){
+                    frictionMap[i][j] = 0.1
+                }else if (red > 230 && red < 250  ){
+                    frictionMap[i][j] = 0.4
+                }else if (red > 250 && alpha == 255 ){
+                    frictionMap[i][j] = 0.6
+                }else if (red == 255 &&  alpha == 255){
+                    frictionMap[i][j] = 1.0
+                }else {
+                    frictionMap[i][j] = 1.0
                 }
                 currentPixel += 1
                 //print("currentPixel: ",currentPixel, "alpha:" ,alpha, "frictionMap:" ,matrix, "i:" ,i, "j:" ,j)
             }
         }
-        print("frictionMap:" ,frictionMap)
+//        print("frictionMap:" ,frictionMap)
     }
     
     
     override func didMove(to view: SKView) {
         print("3")
         logPixelsOf(#imageLiteral(resourceName: "obstaclex4"))
-        setFriction(#imageLiteral(resourceName: "friction"))
+        setFriction(#imageLiteral(resourceName: "NewFrictionMap"))
         
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(GameScene.increaseTimer), userInfo: nil, repeats: true)
         
@@ -169,41 +172,10 @@ class GameScene: SKScene {
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-    }
-    
     override func update(_ currentTime: TimeInterval) {
-        print("4")
         if let gravityX = manager?.deviceMotion?.gravity.x, let gravityY = manager?.deviceMotion?.gravity.y, ball != nil {
-            //18.12.2017 Faris Commented
             
-            let friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
+            let friction = frictionMap[Int(ball.position.x) + 375][Int(ball.position.y) + 667]
             
             
             // let newPosition = CGPoint(x: Double(ball.position.x) + gravityX * 35.0, y: Double(ball.position.y) + gravityY * 35.0)
@@ -214,15 +186,11 @@ class GameScene: SKScene {
             // ball.physicsBody?.applyForce(CGVector(dx: CGFloat(gravityX) * 5000.0, dy: CGFloat(gravityY) * 5000.0))
             
             
-            // var friction = frictionMap[Int(ball.position.x)][Int(ball.position.y)]
-            /* 18.12.2017 Faris Commented
-             ball.physicsBody?.applyImpulse(CGVector(dx: CGFloat(gravityX) * 200.0 * CGFloat(friction), dy: CGFloat(gravityY) * 200.0 * CGFloat(friction)))
-             */
+            ball.physicsBody?.applyImpulse(CGVector(dx: CGFloat(gravityX) * 200.0  * CGFloat(friction), dy: CGFloat(gravityY) * 200.0 * CGFloat(friction)))
         }
     }
     
     func centerBall() {
-        print("5")
         ball.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
         let moveAction = SKAction.move(to: CGPoint(x: frame.midX, y: frame.midY), duration: 0.0)
         ball.run(moveAction)
@@ -231,12 +199,10 @@ class GameScene: SKScene {
     // MARK: - Timer Methods
     
     @objc func increaseTimer() {
-        print("6")
         seconds = (seconds ?? 0.0) + 0.01
     }
     
     func resetTimer() {
-        print("7")
         seconds = 0.0
     }
     
