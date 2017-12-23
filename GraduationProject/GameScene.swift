c4908133a700f02f16303751a46b46b9d8fb819d
obukte <omer.bukte@ozu.edu.tr>
2017-12-23 23:07:50 +0300

    FrictionWork
    Some work in order to main friction done by Ali and me.

--- GraduationProject/GameScene.swift
+++ GraduationProject/GameScene.swift
@@ -125,11 +125,11 @@ class GameScene: SKScene {
                 let blue = B(x: color)
                 let alpha = A(x: color)
                 if (red < 150 ){
-                    frictionMap[i][j] = 0.1
+                    frictionMap[i][j] = 0.9
                 }else if (red > 230 && red < 250  ){
-                    frictionMap[i][j] = 0.4
+                    frictionMap[i][j] = 0.7
                 }else if (red > 250 && alpha == 255 ){
-                    frictionMap[i][j] = 0.6
+                    frictionMap[i][j] = 0.5
                 }else if (red == 255 &&  alpha == 255){
                     frictionMap[i][j] = 1.0
                 }else {
@@ -142,104 +142,104 @@ class GameScene: SKScene {
 //        print("frictionMap:" ,frictionMap)
     }
     
-    func setHeight(_ image: UIImage) {
-        
-        let inputCGImage = image.cgImage
-        let width: Int = (inputCGImage?.width)!
-        let height: Int = (inputCGImage?.height)!
-        let bytesPerPixel: Int = 4
-        let bytesPerRow: Int = bytesPerPixel * width
-        let bitsPerComponent: Int = 8
-        let pixels = UnsafeMutablePointer<UInt32>.allocate(capacity: (width * height))
-        let bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
-        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixels, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
-        context?.draw(inputCGImage!, in: CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
-        
-        func Mask8(x: Int) -> Int {
-            return (x & 0xff)
-        }
-        func R(x: Int) -> Int {
-            return Mask8(x: x)
-        }
-        func G(x: Int) -> Int {
-            return (Mask8(x: x >> 8))
-        }
-        func B(x: Int) -> Int {
-            return (Mask8(x: x >> 16))
-        }
-        func A(x: Int) -> Int {
-            return (Mask8(x: x >> 24))
-        }
-        
-        var currentPixel = pixels
-        for j in 0..<height {// y coordinate
-            for i in 0..<width {// x coordinate
-                let color = currentPixel.pointee.hashValue
-                let red = R(x: color)
-                let green = G(x: color)
-                let blue = B(x: color)
-                let alpha = A(x: color)
-                if (blue < 10 ){
-                    heightMap[i][j] = 10
-                }else if (blue > 10 && blue < 20  ){
-                    heightMap[i][j] = 9
-                }else if (blue > 20 && blue < 30  ){
-                    heightMap[i][j] = 8
-                }else if (blue > 30 &&  blue < 40){
-                    heightMap[i][j] = 7
-                }else if (blue > 40 &&  blue < 50){
-                    heightMap[i][j] = 6
-                }else if (blue > 50 &&  blue < 60){
-                    heightMap[i][j] = 5
-                }else if (blue > 60 &&  blue < 70){
-                    heightMap[i][j] = 4
-                }else if (blue > 70 &&  blue < 80){
-                    heightMap[i][j] = 3
-                }else if (blue > 80 &&  blue < 90){
-                    heightMap[i][j] = 2
-                }else if (blue > 90 &&  blue < 100){
-                    heightMap[i][j] = 1
-                }else {
-                    heightMap[i][j] = 0
-                }
-                currentPixel += 1
-            }
-        }
-        
-        
-        //20.12.2017
-        var currentBallPositionX = ball.position.x
-        var backBallPositionX = ball.position.x-1
-        var nextBallPositionX = ball.position.x+1
-        
-        var currentBallPositionY = ball.position.y
-        var backBallPositionY = ball.position.y-1
-        var nextBallPositionY = ball.position.y+1
-        
-        let heightOfPos = heightMap[Int(ball.position.x) + 375][Int(ball.position.y) + 667]
-        var x: Int?
-        var y: Int?
-        var heightDifference: Int?
-        var comparedDifference = 0
-        
-        for i in 0...2{
-            for j in 0...2{
-                if(i != 1 && j != 1 ){
-                    let nextHeight = heightMap[i-1][j-1]// [x-1][y-1], [x-1][y+1], [x+1][y-1], [x+1][y+1]
-                    heightDifference = heightOfPos - nextHeight
-                    
-                    if(heightDifference! > comparedDifference){
-                        
-                        x = i-1
-                        y = j-1
-                        comparedDifference = heightDifference!
-                        //velocity
-                    }
-                }
-            }
-        }
-    }
+//    func setHeight(_ image: UIImage) {
+//
+//        let inputCGImage = image.cgImage
+//        let width: Int = (inputCGImage?.width)!
+//        let height: Int = (inputCGImage?.height)!
+//        let bytesPerPixel: Int = 4
+//        let bytesPerRow: Int = bytesPerPixel * width
+//        let bitsPerComponent: Int = 8
+//        let pixels = UnsafeMutablePointer<UInt32>.allocate(capacity: (width * height))
+//        let bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+//        let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+//        let context = CGContext(data: pixels, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+//        context?.draw(inputCGImage!, in: CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
+//
+//        func Mask8(x: Int) -> Int {
+//            return (x & 0xff)
+//        }
+//        func R(x: Int) -> Int {
+//            return Mask8(x: x)
+//        }
+//        func G(x: Int) -> Int {
+//            return (Mask8(x: x >> 8))
+//        }
+//        func B(x: Int) -> Int {
+//            return (Mask8(x: x >> 16))
+//        }
+//        func A(x: Int) -> Int {
+//            return (Mask8(x: x >> 24))
+//        }
+//
+//        var currentPixel = pixels
+//        for j in 0..<height {// y coordinate
+//            for i in 0..<width {// x coordinate
+//                let color = currentPixel.pointee.hashValue
+//                let red = R(x: color)
+//                let green = G(x: color)
+//                let blue = B(x: color)
+//                let alpha = A(x: color)
+//                if (blue < 10 ){
+//                    heightMap[i][j] = 10
+//                }else if (blue > 10 && blue < 20  ){
+//                    heightMap[i][j] = 9
+//                }else if (blue > 20 && blue < 30  ){
+//                    heightMap[i][j] = 8
+//                }else if (blue > 30 &&  blue < 40){
+//                    heightMap[i][j] = 7
+//                }else if (blue > 40 &&  blue < 50){
+//                    heightMap[i][j] = 6
+//                }else if (blue > 50 &&  blue < 60){
+//                    heightMap[i][j] = 5
+//                }else if (blue > 60 &&  blue < 70){
+//                    heightMap[i][j] = 4
+//                }else if (blue > 70 &&  blue < 80){
+//                    heightMap[i][j] = 3
+//                }else if (blue > 80 &&  blue < 90){
+//                    heightMap[i][j] = 2
+//                }else if (blue > 90 &&  blue < 100){
+//                    heightMap[i][j] = 1
+//                }else {
+//                    heightMap[i][j] = 0
+//                }
+//                currentPixel += 1
+//            }
+//        }
+//
+//
+//        //20.12.2017
+//        var currentBallPositionX = ball.position.x
+//        var backBallPositionX = ball.position.x-1
+//        var nextBallPositionX = ball.position.x+1
+//
+//        var currentBallPositionY = ball.position.y
+//        var backBallPositionY = ball.position.y-1
+//        var nextBallPositionY = ball.position.y+1
+//
+//        let heightOfPos = heightMap[Int(ball.position.x) + 375][Int(ball.position.y) + 667]
+//        var x: Int?
+//        var y: Int?
+//        var heightDifference: Int?
+//        var comparedDifference = 0
+//
+//        for i in 0...2{
+//            for j in 0...2{
+//                if(i != 1 && j != 1 ){
+//                    let nextHeight = heightMap[i-1][j-1]// [x-1][y-1], [x-1][y+1], [x+1][y-1], [x+1][y+1]
+//                    heightDifference = heightOfPos - nextHeight
+//
+//                    if(heightDifference! > comparedDifference){
+//
+//                        x = i-1
+//                        y = j-1
+//                        comparedDifference = heightDifference!
+//                        //velocity
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     
     
@@ -247,7 +247,7 @@ class GameScene: SKScene {
         print("3")
         logPixelsOf(#imageLiteral(resourceName: "obstaclex4"))
         setFriction(#imageLiteral(resourceName: "NewFrictionMap"))
-        setHeight(#imageLiteral(resourceName: "HeightMap"))
+//        setHeight(#imageLiteral(resourceName: "HeightMap"))
         
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(GameScene.increaseTimer), userInfo: nil, repeats: true)
         
@@ -257,7 +257,7 @@ class GameScene: SKScene {
         ball = SKSpriteNode(imageNamed: "Ball")
         ball.position = CGPoint(x: frame.midX, y: frame.midY)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.height / 2.0)
-        ball.physicsBody?.mass = 4.5
+        ball.physicsBody?.mass = 1.0
         ball.physicsBody?.allowsRotation = false
         ball.physicsBody?.isDynamic = true // necessary to detect collision
         ball.physicsBody?.affectedByGravity = false
@@ -265,7 +265,7 @@ class GameScene: SKScene {
         
         manager = CMMotionManager()
         if let manager = manager, manager.isDeviceMotionAvailable {
-            manager.deviceMotionUpdateInterval = 0.01
+            manager.deviceMotionUpdateInterval = 0.001
             manager.startDeviceMotionUpdates()
         }
     }
@@ -274,17 +274,37 @@ class GameScene: SKScene {
         if let gravityX = manager?.deviceMotion?.gravity.x, let gravityY = manager?.deviceMotion?.gravity.y, ball != nil {
             
             let friction = frictionMap[Int(ball.position.x) + 375][Int(ball.position.y) + 667]
+            let frictionX =  (ball.physicsBody?.mass)! * CGFloat(9.8) * CGFloat(friction)
+            let impulseX = CGFloat(gravityX) * (ball.physicsBody?.mass)! * CGFloat(9.8)
+            let impulseY = CGFloat(gravityY) * (ball.physicsBody?.mass)! * CGFloat(9.8)
+            
+            
             
             
             // let newPosition = CGPoint(x: Double(ball.position.x) + gravityX * 35.0, y: Double(ball.position.y) + gravityY * 35.0)
             // let moveAction = SKAction.moveTo(newPosition, duration: 0.0)
             // ball.runAction(moveAction)
             
-            // applyImpulse() is much better than applyForce()
-            // ball.physicsBody?.applyForce(CGVector(dx: CGFloat(gravityX) * 5000.0, dy: CGFloat(gravityY) * 5000.0))
+//             applyImpulse() is much better than applyForce()
+//             ball.physicsBody?.applyForce(CGVector(dx: CGFloat(gravityX) * (ball.physicsBody?.mass)! * CGFloat(9.8) , dy: CGFloat(gravityY) * (ball.physicsBody?.mass)! * CGFloat(9.8)))
             
-            
-            ball.physicsBody?.applyImpulse(CGVector(dx: CGFloat(gravityX) * 200.0  * CGFloat(friction), dy: CGFloat(gravityY) * 200.0 * CGFloat(friction)))
+            if friction < 1.0 {
+                if impulseX < 0.0 && impulseY < 0.0 {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: impulseX + frictionX , dy: impulseY + frictionX))
+                } else if impulseX > 0.0 && impulseY < 0.0 {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: impulseX - frictionX , dy: impulseY + frictionX))
+                } else if impulseX > 0.0 && impulseY > 0.0 {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: impulseX - frictionX , dy: impulseY - frictionX))
+                } else if impulseX < 0.0 && impulseY > 0.0 {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: impulseX + frictionX , dy: impulseY - frictionX))
+                }
+                print("friction \(frictionX)")
+                print("impulse \(impulseX)")
+            } else {
+                ball.physicsBody?.applyImpulse(CGVector(dx: impulseX , dy: impulseY))
+            }
+
+
         }
     }
     
@@ -297,7 +317,7 @@ class GameScene: SKScene {
     // MARK: - Timer Methods
     
     @objc func increaseTimer() {
-        seconds = (seconds ?? 0.0) + 0.01
+        seconds = (seconds ?? 0.0) + 0.001
     }
     
     func resetTimer() {
