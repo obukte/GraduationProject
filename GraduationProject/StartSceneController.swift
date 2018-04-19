74787ccbd7765e9d4e8ff49c8ccaf749c39cde82
Ali Faris Akdem <faris.akdem@ozu.edu.tr>
2018-04-19 03:36:40 -0700

    Start Button is added
    Start Button is added

---
+++ GraduationProject/StartSceneController.swift
@@ -0,0 +1,14 @@
+//
+//  StartSceneController.swift
+//  GraduationProject
+//
+//  Created by Ali Akdem on 18.04.2018.
+//  Copyright © 2018 Omer Bukte. All rights reserved.
+//
+/*
+import Foundation
+class StartSceneController: UIViewController {
+
+
+}
+*/
